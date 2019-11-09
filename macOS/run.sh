e4a0aa1cff40600e0819d42e9c2996b96df379b6
jeroen-manders <jeroen.manders@external.mckinsey.com>
2019-11-09 16:01:14 +0100

    Fixes

--- macOS/run.sh
+++ macOS/run.sh
@@ -2,8 +2,29 @@
 
 set -euo pipefail;
 
-VERSION="$(cat ../VERSION)";
+INFRAXYS_ROOT_DIR="/opt/infraxys/developer-dev";
+VERSION="$(cat VERSION)";
 IMAGE="quay.io/jeroenmanders/infraxys-developer-installer:$VERSION";
 
+if [ -d "$INFRAXYS_ROOT_DIR" ]; then
+  echo "Directory $INFRAXYS_ROOT_DIR already exists. Not making any changes to it.";
+  exit 1;
+fi;
+
+mkdir -p "$INFRAXYS_ROOT_DIR";
+
 docker pull $IMAGE;
-docker run -it --rm $IMAGE
+docker run -it --rm \
+  -e "VERSION=$VERSION" \
+  -e "INFRAXYS_ROOT_DIR=$INFRAXYS_ROOT_DIR" \
+  -v $INFRAXYS_ROOT_DIR:/infraxys-root:rw \
+  $IMAGE
+
+username="$(id -un)";
+groupname="$(id -gn)";
+
+echo "Setting owner of Infraxys files to $username:$groupname";
+chown -R "$username":"$groupname" "$INFRAXYS_ROOT_DIR";
+
+cd "$INFRAXYS_ROOT_DIR/bin";
+./up.sh;
